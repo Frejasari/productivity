5957f212c47a0b984ab35f02abc232200390f44e
Sari <freja.sender@gmail.com>
2018-06-25 14:37:21 +0200

    reformat and organize app.js

--- app.js
+++ app.js
@@ -1,85 +1,98 @@
-require('dotenv').config();
+//#region Import Packages
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
+//#endregion
 
+//#region setup Database
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/productivity', {useMongoClient: true})
+  .connect(
+    "mongodb://localhost/productivity",
+    { useMongoClient: true }
+  )
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
+//#endregion
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+//#region app and middleware setup
+const app_name = require("./package.json").name;
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
 
 const app = express();
 
-// Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//#endregion
 
-// Express View engine setup
+//#region Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
+hbs.registerHelper("ifUndefined", (value, options) => {
+  if (arguments.length < 2) throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+  if (typeof value !== undefined) {
+    return options.inverse(this);
   } else {
-      return options.fn(this);
+    return options.fn(this);
   }
 });
-  
+//#endregion
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+//#region set locals
+app.locals.title = "Level up your Productivity";
+//#endregion
 
-
+//#region enable authentication with sessions and passport
 // Enable authentication using session + passport
-app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
+app.use(
+  session({
+    secret: "irongenerator",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 app.use(flash());
-require('./passport')(app);
-    
+require("./passport")(app);
+//#endregion
 
-const index = require('./routes/index');
-app.use('/', index);
+//#region Routes
+const index = require("./routes/index");
+app.use("/", index);
 
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
-      
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+
+
+//#endregion
 
 module.exports = app;
