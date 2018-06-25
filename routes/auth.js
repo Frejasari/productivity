339e90b022c17f6ff397148d2f2091692e91f0dc
Sari <freja.sender@gmail.com>
2018-06-25 16:05:06 +0200

    formatting

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const authRoutes = express.Router();
 const User = require("../models/User");
 
@@ -7,17 +7,19 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
 authRoutes.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
-authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+authRoutes.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 authRoutes.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -47,7 +49,7 @@ authRoutes.post("/signup", (req, res, next) => {
       role:"teacher"
     });
 
-    newUser.save((err) => {
+    newUser.save(err => {
       if (err) {
         res.render("auth/signup", { message: "Something went wrong" });
       } else {
