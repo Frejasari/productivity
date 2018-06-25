88cc19a31a19162909458a4d0349c9856e48240b
Sari <freja.sender@gmail.com>
2018-06-25 17:25:05 +0200

    check for duplicate email & username at signup
    + reformat file
    
    + WIP email is unique -- no empty field allowed!

--- routes/auth.js
+++ routes/auth.js
@@ -7,28 +7,24 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-authRoutes.get("/login", (req, res, next) => {
-  res.render("auth/login", { message: req.flash("error") });
-});
-
 authRoutes.post(
   "/login",
   passport.authenticate("local", {
     successRedirect: "/",
-    failureRedirect: "/auth/login",
+    failureRedirect: "/",
     failureFlash: true,
     passReqToCallback: true
   })
 );
 
+//#region signup
 authRoutes.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
 authRoutes.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const rol = req.body.role;
+  const { username, email, password } = req.body;
+
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
@@ -36,32 +32,49 @@ authRoutes.post("/signup", (req, res, next) => {
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
+      console.log("The username already exists");
       res.render("auth/signup", { message: "The username already exists" });
       return;
     }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      password: hashPass,
-      role:"teacher"
-    });
-
-    newUser.save(err => {
-      if (err) {
-        res.render("auth/signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("/");
+    // check if email is duplicate
+    User.findOne({ email }).then(user => {
+      if (email !== "" && user !== null) {
+        res.render("auth/signup", { message: "There is already an account with this email" });
+        return;
       }
+      createAndSaveNewUser(username, password, email)
+        .then(user => {
+          res.redirect("/");
+        })
+        .catch(err => {
+          console.log(err);
+          res.render("auth/signup", { message: "Something went wrong" });
+        });
     });
   });
 });
+//#endregion
 
+//#region logout
 authRoutes.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+//#endregion
+
+//#region Database utility functions
+function createAndSaveNewUser(username, password, email) {
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashPass
+  });
+
+  return newUser.save();
+}
+//#endregion
 
 module.exports = authRoutes;
