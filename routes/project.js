12ffc2e6b1e6aa9bd52f6860e2ec767dd34beb4b
Sari <freja.sender@gmail.com>
2018-06-26 18:33:33 +0200

    update route to specific project

--- routes/project.js
+++ routes/project.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Project = require("../models/Project");
 const TaskPackage = require("../models/TaskPackage");
 
-router.get("/userName/:project", (req, res, next) => {
+router.get("/:projectId", (req, res, next) => {
   res.render("project/project");
   // Project.findById(req.params.project).then(project => {
   // });
