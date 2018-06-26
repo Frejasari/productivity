a1595638b2f90391379be384984aac54ef0918d0
Sari <freja.sender@gmail.com>
2018-06-26 12:01:53 +0200

    format files

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,25 +1,27 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User          = require('../models/User');
-const bcrypt        = require('bcrypt');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, foundUser) => {
-    if (err) {
-      next(err);
-      return;
-    }
+passport.use(
+  new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, foundUser) => {
+      if (err) {
+        next(err);
+        return;
+      }
 
-    if (!foundUser) {
-      next(null, false, { message: 'Incorrect username' });
-      return;
-    }
+      if (!foundUser) {
+        next(null, false, { message: "Incorrect username" });
+        return;
+      }
 
-    if (!bcrypt.compareSync(password, foundUser.password)) {
-      next(null, false, { message: 'Incorrect password' });
-      return;
-    }
-
-    next(null, foundUser);
-  });
-}));
+      if (!bcrypt.compareSync(password, foundUser.password)) {
+        next(null, false, { message: "Incorrect password" });
+        return;
+      }
+      console.log(("LocalStrategy", foundUser));
+      next(null, foundUser);
+    });
+  })
+);
