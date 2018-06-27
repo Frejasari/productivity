cf46d484185287ae60da94d78255711a785ea0a1
Sari <freja.sender@gmail.com>
2018-06-27 13:06:08 +0200

    change route for specific project so it does not match any other simple pattern

--- routes/project.js
+++ routes/project.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Project = require("../models/Project");
 const TaskPackage = require("../models/TaskPackage");
 
-router.get("/:projectId", (req, res, next) => {
+router.get("/show/:projectId", (req, res, next) => {
   Project.findById(req.params.projectId).then(project => {
     TaskPackage.find({'_id': { $in: project._taskPackages}})
     .then( packages => {
