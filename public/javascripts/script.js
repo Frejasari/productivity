84f05589e18f842a592b4ca6107700e5869cd6e8
Sari <freja.sender@gmail.com>
2018-06-27 14:45:44 +0200

    set start date by default to today

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    let startDatePicker = document.getElementById("project-start-date");
+    if (startDatePicker) startDatePicker.value = new Date().toDateInputValue();
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+Date.prototype.toDateInputValue = function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  return local.toJSON().slice(0, 10);
+};
