bb1562cb04506c5d80ecbbb8fe6fc5615823b7fc
steffriedrichs <steffi.friedrichs@web.de>
2018-06-28 18:31:18 +0200

    removed style sheets

--- routes/profile.js
+++ routes/profile.js
@@ -8,4 +8,32 @@ router.get("/", (req, res, next) => {
   });
 });
 
+router.get("/user-settings/:userId", (req, res, next) => {
+  let parentSite;
+  req.rawHeaders.forEach( element => {
+    if(element.includes('http://')){
+      parentSite = element;
+    }
+  })
+  console.log(parentSite);
+  res.render("profile/user-settings", {parentSite: "/profile"});
+});
+
+router.post("/user-settings/:userId", (req, res, next) => {
+  // save changes in dtbase:
+
+
+
+
+  console.log("REQ BODY: ",req.body);
+  // { username: 'Anna',
+  // email: 'anna.testuser@abc.de',
+  // newPassword: '',
+  // newPasswordConfirm: '',
+  // imgUrl: '/images/default-silhouette.jpg' }
+
+  res.redirect("/profile");
+});
+
 module.exports = router;
+
