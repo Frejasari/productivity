5bca474d4e4e837213bffc7707b61193c7a20238
Sari <freja.sender@gmail.com>
2018-06-29 10:17:34 +0200

    format routes/profiles

--- routes/profile.js
+++ routes/profile.js
@@ -25,53 +25,51 @@ function createAndSaveNewIdea(package, userId) {
 }
 
 //#endregion
+
+//#region user-settings
 router.get("/user-settings/:userId", (req, res, next) => {
   let parentSite;
-  req.rawHeaders.forEach( element => {
-    if(element.includes('http://')){
+  req.rawHeaders.forEach(element => {
+    if (element.includes("http://")) {
       parentSite = element;
     }
-  })
+  });
   console.log(parentSite);
-  res.render("profile/user-settings", {parentSite: "/profile"});
+  res.render("profile/user-settings", { parentSite: "/profile" });
 });
 
 router.post("/user-settings/:userId", (req, res, next) => {
-
   // Bcrypt to encrypt passwords
   const bcrypt = require("bcrypt");
   const salt = bcrypt.genSaltSync(10);
 
-  User.findById(req.params.userId).then(user => {
-    
-  // check, if password was correct:
-  if(!bcrypt.compareSync(req.body.password, user.password)){
+  User.findById(req.params.userId)
+    .then(user => {
+      // check, if password was correct:
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        // send error message!!
+        console.log("NOT CORRECT PASSWORD");
 
-    // send error message!!
-    console.log("NOT CORRECT PASSWORD");
+        // check, if two new passwords are equal:
+      } else if (req.body.newPassword !== req.body.newPasswordConfirm) {
+        // send error message!!
+        console.log("ENTERED NEW PASSWORDS ARE NOT EQUAL!!");
+      } else {
+        // change user properties and save in db
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.imgUrl = req.body.imgUrl;
+        user.password = bcrypt.hashSync(req.body.newPassword, salt);
 
-  // check, if two new passwords are equal:
-  }else if(req.body.newPassword !== req.body.newPasswordConfirm){
-    
-    // send error message!!
-    console.log("ENTERED NEW PASSWORDS ARE NOT EQUAL!!");
+        user.save();
 
-  }else{
-    // change user properties and save in db
-    user.username = req.body.username;
-    user.email    = req.body.email;  
-    user.imgUrl   = req.body.imgUrl;
-    user.password = bcrypt.hashSync(req.body.newPassword, salt);
-
-    user.save();
-
-    res.redirect("/profile");
-  }
-  })
-  .catch(err => {
-    console.log(err);
-  });
+        res.redirect("/profile");
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+//#endregion
 
 module.exports = router;
-
